24f8dcaa85147251d08dec866cd2f470264ad585
MohdFaraz <farazahmad280@gmail.com>
2025-07-07 15:18:49 +0530

    Backup from Live

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,3 +1,13 @@
+<!-- marquee tag START  -->
+<marquee class="marque-moved" behavior="" direction="left">
+        <ul class="new-offers">
+            <li> <span class="pg-walfare" > <img class="caps" src="{{ asset('front') }}/assets/img/cap.png" alt="cap">
+ GYANDEEP WELFARE & REHABILITATION SOCIETY DASUYA (PB)</span> <span>RCI CODE:PB019 </span> <span class="open-details" >ADMISSION OPEN (2025-26)</span> <span class="main-edu"> D.Ed In SPL. Education (IDD) <span class="new-spns">APPLY NOW</span>  </span>    </li>
+        </ul>
+        
+    </marquee>
+<!-- END  -->
+
 <!-- FOOTER SECTION START -->
 <footer class="ul-footer">
        <div class="whats-float">
@@ -19,8 +29,8 @@
           </div>
           <!-- txt -->
           <div class="ul-footer-top-contact-info-txt">
-            <span class="ul-footer-top-contact-info-label">Address</span>
-            <h5 class="ul-footer-top-contact-info-address">H.No. 538/1, Ward 7, Kirpal Colony, Dasuya, District
+            <span class="ul-footer-top-contact-info-label text-start">Address</span>
+            <h5 class="ul-footer-top-contact-info-address text-start">H.No. 538/1, Ward 7, Kirpal Colony, Dasuya, District
               Hoshiarpur, Punjab – 144205</h5>
           </div>
         </div>
@@ -35,8 +45,8 @@
           </div>
           <!-- txt -->
           <div class="ul-footer-top-contact-info-txt">
-            <span class="ul-footer-top-contact-info-label">Send Email</span>
-            <h5 class="ul-footer-top-contact-info-address"><a href="mailto:info@gyandeep.ngo">info@gyandeep.ngo</a>
+            <span class="ul-footer-top-contact-info-label text-start">Send Email</span>
+            <h5 class="ul-footer-top-contact-info-address text-start"><a href="mailto:info@gyandeep.ngo">info@gyandeep.ngo</a>
             </h5>
           </div>
         </div>
@@ -51,8 +61,8 @@
           </div>
           <!-- txt -->
           <div class="ul-footer-top-contact-info-txt">
-            <span class="ul-footer-top-contact-info-label">Call Emergency</span>
-            <h5 class="ul-footer-top-contact-info-address"><a href="tel:919711908590">+919711908590</a></h5>
+            <span class="ul-footer-top-contact-info-label text-start">Call Emergency</span>
+            <h5 class="ul-footer-top-contact-info-address text-start"><a href="tel:919711908590">+919711908590</a></h5>
           </div>
         </div>
       </div>
@@ -62,11 +72,11 @@
   <div class="ul-footer-middle">
     <div class="ul-footer-container">
       <div class="ul-footer-middle-wrapper wow animate__fadeInUp">
-        <div class="ul-footer-about">
+        <div class="ul-footer-about text-start">
           <a href="{{ route('home') }}">
             <h5 class="main-logo"> <img src="{{ asset('front') }}/assets/img/plant.png" alt=""></h5>
           </a>
-          <p class="ul-footer-about-txt">A registered non-profit organization committed to the welfare, education, and
+          <p class="ul-footer-about-txt text-start">A registered non-profit organization committed to the welfare, education, and
             rehabilitation of differently-abled individuals</p>
           <div class="ul-footer-socials">
             <a href="https://www.facebook.com/www.gyandeep.ngo"><i class="flaticon-facebook"></i></a>
@@ -77,9 +87,9 @@
         </div>
 
         <div class="ul-footer-widget">
-          <h3 class="ul-footer-widget-title">Quick Links</h3>
+          <h3 class="ul-footer-widget-title text-start">Quick Links</h3>
 
-          <div class="ul-footer-widget-links">
+          <div class="ul-footer-widget-links text-start">
             <a href="{{ route('about') }}">About Us</a>
             <a href="{{ route('services') }}">Our Services</a>
             <a href="{{ route('blog') }}">Our Blogs</a>
@@ -89,7 +99,7 @@
         </div>
 
         <div class="ul-footer-widget ul-footer-recent-posts">
-          <h3 class="ul-footer-widget-title">Recent Posts</h3>
+          <h3 class="ul-footer-widget-title text-start">Recent Posts</h3>
 
           <div class="ul-blog-sidebar-posts">
             <!-- single post -->
@@ -100,36 +110,36 @@
               </div>
 
               <div class="txt">
-                <span class="date">
+                <span class="date text-start">
                   <span class="icon"><i class="flaticon-calendar"></i></span>
                   <span>May 12, 2025</span>
                 </span>
 
-                <h4 class="title"><a href="{{ route('blog.detail', ['blog_slug' => 'blog-details']) }}">Breaking
+                <h4 class="title text-start"><a href="{{ route('blog.detail', ['blog_slug' => 'blog-details']) }}">Breaking
                     Barriers with Inclusive Education.</a></h4>
               </div>
             </div>
 
             <!-- single post -->
             <div class="ul-blog-sidebar-post ul-footer-post">
-              <div class="img">
+              <div class="img text-start">
                 <img src="{{ asset('front') }}/assets/img/donation-2.jpeg" alt="Post Image">
               </div>
 
-              <div class="txt">
+              <div class="txt text-start">
                 <span class="date">
                   <span class="icon"><i class="flaticon-calendar"></i></span>
                   <span>May 12, 2025</span>
                 </span>
 
-                <h4 class="title"><a href="{{ route('blog.detail', ['blog_slug' => 'blog-details-one']) }}">How
+                <h4 class="title text-start"><a href="{{ route('blog.detail', ['blog_slug' => 'blog-details-one']) }}">How
                     Therapy Transforms Young Lives</a></h4>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="ul-footer-widget ul-nwsltr-widget">
+        <div class="ul-footer-widget ul-nwsltr-widget text-start">
           <h3 class="ul-footer-widget-title">Contact Us</h3>
           <div class="ul-footer-widget-links ul-footer-contact-links">
             <a href="mailto:info@gyandeep.ngo"><i class="flaticon-mail"></i> info@gyandeep.ngo</a>
